12686cae63661a26de55c6db2066d8a7f6e1c577
tarinya <muriele@trentini.ch>
2016-10-19 14:12:45 +0200

    deletes unused files

--- androidstudio_project/app/src/main/java/ch/hsr/mge/learnit/presentation/CardSelectionListener.java
+++
@@ -1,5 +0,0 @@
-package ch.hsr.mge.learnit.presentation;
-
-public interface CardSelectionListener {
-    void onItemSelected(int position);
-}
--- androidstudio_project/app/src/main/java/ch/hsr/mge/learnit/presentation/CardSetSelectionListener.java
+++
@@ -1,5 +0,0 @@
-package ch.hsr.mge.learnit.presentation;
-
-public interface CardSetSelectionListener {
-    void onItemSelected(int position);
-}
