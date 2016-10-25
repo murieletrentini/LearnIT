35fc0bbef38d94b0b453e0fce580fb26c231825c
Nico Vinzens <vinzensnico@hotmail.com>
2016-10-25 19:21:00 +0200

    db now behaves as expected :D:D

--- androidstudio_project/app/src/main/java/ch/hsr/mge/learnit/database/DBHelper.java
+++ androidstudio_project/app/src/main/java/ch/hsr/mge/learnit/database/DBHelper.java
@@ -67,8 +67,8 @@ public class DBHelper extends SQLiteOpenHelper {
     public CardSet getCardSet(String name) {
         CardSet set = new CardSet(name);
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery( "SELECT * FROM " + CARDSET_TABLE_NAME + " WHERE " +
-                CARDSET_COLUMN_NAME + "=?", new String[] { name });
+        Cursor cursor = db.rawQuery( "SELECT * FROM " + CARD_TABLE_NAME+ " WHERE " +
+                CARD_COLUMN_CARDSETNAME + "=?", new String[] { name });
         if (cursor.moveToFirst()) {
             while (!cursor.isAfterLast()) {
                 Card card = new Card();
